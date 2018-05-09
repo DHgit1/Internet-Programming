826f2740fbf4a7ed122fd821c1eb3bf1bbcb8d54
Devin Hight <dhight@csumb.edu>
2018-05-09 06:48:58 +0000

    Final Project

--- final/deleteProduct.php
+++ final/deleteProduct.php
@@ -5,11 +5,11 @@
     }
     include '../dbConnection.php';
     $conn = getDatabaseConnection("final");
-    $sql = "DELETE FROM movie_products WHERE Id = ".$_GET['productId'];
+    $sql = "DELETE FROM movie_products WHERE Id = ".$_GET['Id'];
     $stmt = $conn->prepare($sql);
-    $stmt->execute($np);
+    $stmt->execute();
     $sql = "SET @newid=0; UPDATE movie_products SET Id=(@newid:=@newid+1) ORDER BY Id";
     $stmt = $conn->prepare($sql);
-    $stmt->execute($np);
+    $stmt->execute();
     header("Location: admin.php");
 ?>
